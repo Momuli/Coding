e3b7b2dd31f74be153348b1e7d2a4577d0d73c24
Momuli <51191439+Momuli@users.noreply.github.com>
2022-02-19 18:58:09 +0800

    Add files via upload

---
+++ 岛屿问题/200_岛屿数量.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        res = 0  # 用于记录岛屿总数
+        # 遍历二维数组中的每一个元素
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    res += 1     # 岛屿数+1
+                    self.fill_0(grid, i, j)
+        return res
+
+    # 将陆地(1)用海水(0)淹没      # 如果能形成岛屿,那么陆地的数目大于等于1,并且是相邻的
+    def fill_0(self, grid, row, col):
+        # base case
+        # 1.索引超出边界
+        if (row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0])):
+            return
+
+        # 2.grid[col][row]已经 == '0'
+        if grid[row][col] == '0':
+            return
+
+        grid[row][col] = '0'
+        self.fill_0(grid, row-1, col)  # 上方
+        self.fill_0(grid, row+1, col)  # 下方
+        self.fill_0(grid, row, col-1)  # 左方
+        self.fill_0(grid, row, col+1)  # 右方
+
+if __name__ == '__main__':
+    grid1 = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+    ]
+    grid2 = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]]
+
+    rel = Solution().numIslands(grid1)
+    print(rel)
